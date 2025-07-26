42ddc3bfb5708fb1f939472a7f333eb4147ccdd0
jangidudi45 <jangidudit45@gmail.com>
2025-07-26 12:46:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -564,6 +564,14 @@ async def upload(bot: Client, m: Message):
 
     await editable.edit("𝗡𝗼𝘄 𝗦𝗲𝗻𝗱 𝗧𝗵𝗲 𝗧𝗵𝘂𝗺𝗯 𝗨𝗿𝗹 𝗘𝗴 » https://graph.org/file/13a89d77002442255efad-989ac290c1b3f13b44.jpg\n\n𝗢𝗿 𝗜𝗳 𝗗𝗼𝗻'𝘁 𝗪𝗮𝗻𝘁 𝗧𝗵𝘂𝗺𝗯𝗻𝗮𝗶𝗹 𝗦𝗲𝗻𝗱 = 𝗻𝗼")
     input6 = message = await bot.listen(editable.chat.id)
+    thumb = input6.text
+    if thumb.lower() == "no":
+        thumb = None
+    elif thumb.startswith("http://") or thumb.startswith("https://"):
+        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
+    else:
+        thumb = None
     raw_text6 = input6.text
     await input6.delete(True)
     await editable.delete()
