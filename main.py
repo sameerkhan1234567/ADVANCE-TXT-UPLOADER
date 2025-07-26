683418e559a7c3f23e50eb43ea742f20b631f4ee
jangidudi45 <jangidudit45@gmail.com>
2025-07-26 14:21:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -603,8 +603,8 @@ async def upload(bot: Client, m: Message):
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
                 
-            elif "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/bhainskipathshala-data/" in url:
-                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/bhainskipathshala-data/", "")
+            elif "transcoded-videos.classx.co.in" in url:
+                url = url.replace("https://transcoded-videos.classx.co.in/videos/rgacademyrajasthan-data/", "")
                 name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "@").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
