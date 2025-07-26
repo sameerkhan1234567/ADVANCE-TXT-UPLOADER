a94602fdf4684ea8c66b331dba87fcedad80fe06
jangidudi45 <jangidudit45@gmail.com>
2025-07-26 13:08:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -568,13 +568,16 @@ async def upload(bot: Client, m: Message):
     await input6.delete(True)
     await editable.delete()
 
-    #thumb = input6.text
-    #if thumb.startswith("http://") or thumb.startswith("https://"):
-        #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        #thumb = "thumb.jpg"
+    thumb = input6.text
+    if thumb.lower() == "no":
+        thumb = None
+    elif thumb.startswith("http://") or thumb.startswith("https://"):
+        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"
     else:
-        thumb == "no"
-    failed_count =0
+        thumb = None
+
+    failed_count = 0
     if len(links) == 1:
         count = 1
     else:
