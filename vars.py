da5585c882ade317725450b0fd87f47978e3f290
jangidudi45 <jangidudit45@gmail.com>
2025-09-04 15:44:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,15 +1,5 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
+import os
 
-from os import environ
-
-API_ID = int(environ.get("API_ID", "22946135")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "93e1b0c387cabe34a3ccfa1724ae8527") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
-
-# Database Configuration for MongoDB
-# Example: "mongodb://user:password@host:port/dbname"
-# For MongoDB Atlas: "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<dbname>?retryWrites=true&w=majority"
-DATABASE_URL = environ.get("DATABASE_URL", "mongodb://localhost:27017/your_bot_db")
-# Replace "your_bot_db" with your desired database name.
-# For local testing, you might use "mongodb://localhost:27017/your_bot_db"
+API_ID = os.environ.get("API_ID", "22946135") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "93e1b0c387cabe34a3ccfa1724ae8527") #Replace With your api hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Replace With your bot token
