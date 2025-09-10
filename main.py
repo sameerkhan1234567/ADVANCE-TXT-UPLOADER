91e8a82c674c204d763b946346455fb8927d132e
jangidudi45 <jangidudit45@gmail.com>
2025-09-10 11:25:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -558,7 +558,7 @@ async def upload(bot: Client, m: Message):
 
     success_count = len(links) - failed_count
     success_count =0
-    if len(links) =1
+    if len(links) ==1:
         failed_count =1
     else:
         count = int(raw_text)
