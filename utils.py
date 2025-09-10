11e516ae7159d628188f80839893b5f85b86ada0
jangidudi45 <jangidudit45@gmail.com>
2025-09-10 09:40:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -88,7 +88,7 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "🟢" * completed_length + "🔴" * remaining_length
+            progress_bar = "▰" * completed_length + "▱" * remaining_length
                 
             try:
                 await reply.edit(f'</b>╭──⌯════🌟𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🌟═════⌯──╮ \n├⚡ {progress_bar}\n ├🚀 𝗦𝗽𝗲𝗲𝗱 ➠ {sp} \n ├📛 𝗣𝗿𝗼𝗴𝗿𝗲𝘀𝘀 ➠ {perc} \n ├📟 𝗟𝗼𝗮𝗱𝗲𝗱 ➠ {cur} \n ├🧲 𝗦𝗶𝘇𝗲 ➠ {tot} \n ├🕑 𝗘𝘁𝗮 ➠ {eta} \n╰─══**✪ 𝗔𝗗𝗜𝗧𝗬𝗔⚡️ *✪**══─╯</b>') 
