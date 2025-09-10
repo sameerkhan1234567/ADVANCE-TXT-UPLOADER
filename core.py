6656934c24d61145272497fe649b45a688e00b95
jangidudi45 <jangidudit45@gmail.com>
2025-09-10 11:50:21 +0530

    Update core.py

--- core.py
+++ core.py
@@ -225,7 +225,7 @@ async def download_video(url, cmd, name):
 
 
 async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
-    reply = await m.reply_text(f"🚀🚀🚀𝗨𝗣𝗟𝗢𝗔𝗗𝗜𝗡𝗚🚀🚀🚀 » `{name}`\n\n🤖𝗕𝗢𝗧 𝗠𝗔𝗗𝗘 𝗕𝗬 ➤ 𝗔𝗗𝗜𝗧𝗬𝗔⚡️ ")
+    reply = await m.reply_text(f"**📤ᴜᴘʟᴏᴀᴅɪɴɢ📤** » `{name}`\n\nʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ᴘɪᴋᴀᴄʜᴜ")
     time.sleep(1)
     start_time = time.time()
     await m.reply_document(ka, caption=cc1)
@@ -239,7 +239,7 @@ async def send_doc(bot: Client, m: Message, cc, ka, cc1, prog, count, name):
 async def send_vid(bot: Client, m: Message, cc, filename, thumb, name, prog):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:12 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete(True)
-    reply = await m.reply_text(f"**:📤ᴜᴘʟᴏᴀᴅɪɴɢ📤** » `{name}`\n\nʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ᴘɪᴋᴀᴄʜᴜ")
+    reply = await m.reply_text(f"**📤ᴜᴘʟᴏᴀᴅɪɴɢ📤** » `{name}`\n\nʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ᴘɪᴋᴀᴄʜᴜ")
     try:
         thumbnail = f"{filename}.jpg" if thumb == "no" else thumb
     except Exception as e:
