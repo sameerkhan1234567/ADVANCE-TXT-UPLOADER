db0cf252d9e24ea36ef9c39687205a750d3186a7
sameerkhan1234567 <officialgamerrj01@gmail.com>
2025-10-05 22:37:27 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -10,6 +10,6 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
 # Database Configuration for MongoDB
 # Example: "mongodb://user:password@host:port/dbname"
 # For MongoDB Atlas: "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<dbname>?retryWrites=true&w=majority"
-DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://officialgamerrj01:q6WDx8GUZNJi9t3Z@cluster0.zlany.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb://localhost:27017/your_bot_db")
 # Replace "your_bot_db" with your desired database name.
 # For local testing, you might use "mongodb://localhost:27017/your_bot_db"
